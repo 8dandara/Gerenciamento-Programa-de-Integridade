c174cf1a3164038d0afd5e1074ed1f1d999d91fc
Dandara gouveia <dandaragouveia8@gmail.com>
2025-05-15 01:25:50 -0300

    Back-end do Sistema de Gerenciamento do Programa de Integridade

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/acoes', require('./routes/acoesRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/pilares', require('./routes/pilaresRoutes'));
+app.use('/api/historico', require('./routes/historicoRoutes'));
+app.use('/api/notificacoes', require('./routes/notificacaoRoutes'));
+app.use('/api/dashboard', require('./routes/dashboardRoutes'));
+
+const { verificarNotificacoes } = require('./utils/notificador');
+verificarNotificacoes(); // executar uma vez na inicialização
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
